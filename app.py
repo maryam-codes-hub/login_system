f8ca12f6259abddd783697402a2b8cca837ec8ab
maryam-codes-hub <maryamehsan308@gmail.com>
2026-09-25 15:02:42 +0500

    login signup html

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("login.html")
+@app.route("/login")
+def login_page():
+        return render_template("login.html")
+@app.route("/signup")
+def signup_page():
+        return render_template("signup.html")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
